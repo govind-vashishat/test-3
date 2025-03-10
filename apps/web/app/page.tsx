520aead99d828d19a630a356eb7d557aa268cc05
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-03-10 18:52:36 +0530

    final test commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      hey there from nextjs
+    <div className="min-h-screen bg-black text-white text-4xl font-bold flex flex-col justify-center items-center">
+      badmashi nahi mittar asla ham bhi rakhte hai
     </div>
   );
 }
