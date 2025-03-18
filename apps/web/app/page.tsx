fb423b1e750f778fd43ddc75d11f82392811ece9
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-03-18 17:56:25 +0530

    .

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white text-4xl font-bold flex flex-col justify-center items-center">
+    <div>
       badmashi nahi mittar asla ham bhi rakhte hai
     </div>
   );
